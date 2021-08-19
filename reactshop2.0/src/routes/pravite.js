82717afb142aa1ea4a9dacbf44b0dde4300a9186
liuyang <515939422@qq.com>
2021-08-19 16:01:13 +0800

    update：日常更新

--- reactshop2.0/src/routes/pravite.js
+++ reactshop2.0/src/routes/pravite.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react'
 import { Redirect, Route } from 'react-router-dom'
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-    <Route {...rest} render={props => { Boolean(localStorage['isLogin']) ? ( <Component { ...props }/> ) : ( <Redirect  to={{  pathname: '/login', state: {from: props.location } }} /> ) } } />
+    <Route
+        {...rest} 
+        render={
+            props => { 
+                Boolean(localStorage['isLogin'])
+                ? ( <Component { ...props }/> )
+                : ( <Redirect  to={{  pathname: '/login', state: {from: props.location } }} /> ) 
+            }
+        } 
+        />
 }
 
 export {
